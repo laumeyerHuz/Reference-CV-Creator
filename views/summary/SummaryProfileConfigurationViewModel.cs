9a4dc61325a895770118f8ef7700a67067b40b45
Leonhard.Laumeyer <Leonhard.Laumeyer@hz.group>
2023-07-05 15:46:11 +0200

    removed unnecessary import

--- views/summary/SummaryProfileConfigurationViewModel.cs
+++ views/summary/SummaryProfileConfigurationViewModel.cs
@@ -4,7 +4,6 @@ using System;
 using System.IO;
 using System.Collections.ObjectModel;
 using System.Text.Json;
-using HandyControl.Controls;
 using System.Windows.Input;
 using CommunityToolkit.Mvvm.Input;
 
